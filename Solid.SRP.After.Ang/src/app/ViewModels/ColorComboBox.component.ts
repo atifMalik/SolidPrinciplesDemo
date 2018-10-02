0a84b878cd57a81e99a34781d1d9f1a455ace1e8
Atif Malik <atifimalik@gmail.com>
2018-10-02 16:18:07 -0400

    Added ColorComboBox bootstrapped View and corresponding ViewModel.

---
+++ Solid.SRP.After.Ang/src/app/ViewModels/ColorComboBox.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@
+Component({
+  selector: 'mn-ColorCombo',
+  templateUrl: '../Views/ColorComboBox.component.html'
+})
+export class ColorComboBoxVM {
+
+}
