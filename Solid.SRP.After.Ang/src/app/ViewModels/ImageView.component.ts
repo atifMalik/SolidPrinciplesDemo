128959bfee6f366cf2d2318978fc41db09f5698f
Atif Malik <atifimalik@gmail.com>
2018-10-26 15:24:36 -0400

    hideImage property added to Image ViewModel, and bound to Image View.

--- Solid.SRP.After.Ang/src/app/ViewModels/ImageView.component.ts
+++ Solid.SRP.After.Ang/src/app/ViewModels/ImageView.component.ts
@@ -12,11 +12,13 @@ import { IColorItem } from "@app/Models/ColorItem";
 export class ImageVM extends BaseVM {
   constructor() {
     super();
+    this.hideImage = true;
     this.currentColorItem = { image: '' } as IColorItem;
     this.mediator.register(this, [Messages.Combobox_Color_Selected, Messages.ListBox_Color_Selected]);
   }
 
   currentColorItem: IColorItem;
+  hideImage: boolean;
 
   messageNotification(message: string, args: object) {
     switch (message) {
@@ -26,5 +28,7 @@ export class ImageVM extends BaseVM {
 
       //default:
     }
+
+    this.hideImage = false;
   }
 }
